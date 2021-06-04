a5f2eeb872cfdf175e021eaf64df38ba27e5d9dd
Jason Carini <carinij@gmail.com>
2021-06-03 19:44:29 -0700

    Added back in the path.join stuff

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const cors = require('cors');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(cors());
 
 app.use('/course', (req, res) => {
