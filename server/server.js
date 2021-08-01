5dc7c6eec479ccee9656a2a61915b5b5d20648e1
mackenziekynoch <mackenziekynoch@gmail.com>
2021-07-31 18:49:01 -0600

    edited for new schema and route changes to reviews service

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('newrelic');
 const shrinkRay = require('shrink-ray-current');
 const express = require('express');
 const path = require('path');
@@ -22,12 +23,12 @@ app.use('/course', (req, res) => {
   res.sendFile('index.html', {root: 'public'});
 });
 
-app.all('/reviews/item', async (req, res) => {
+app.all('/reviews/item/:courseId', async (req, res) => {
   console.log(`proxying request to ${req.originalUrl} with method ${req.method}`);
   await axios({
     method: req.method,
     url: reviewURL + req.originalUrl,
-    data: JSON.stringify(req.body)
+    data: req.body
   })
     .then((results) => {
       res.status(200).json(results.data);
@@ -52,7 +53,7 @@ app.all('/reviews/reviewer/:reviewerId', async (req, res) => {
     });
 });
 
-app.all('/review/item', async (req, res) => {
+app.all('/reviews/item/:courseId/reviewer/:reviewerId', async (req, res) => {
   console.log(`proxying request to ${req.originalUrl} with method ${req.method}`);
   await axios({
     method: req.method,
@@ -67,20 +68,6 @@ app.all('/review/item', async (req, res) => {
     });
 });
 
-app.all('/reviews/item/:courseId', async (req, res) => {
-  console.log(`proxying request to ${req.originalUrl} with method ${req.method}`);
-  await axios({
-    method: req.method,
-    url: reviewURL + req.originalUrl,
-    data: req.body
-  })
-    .then((results) => {
-      res.status(200).json(results);
-    })
-    .catch(err => {
-      res.status(400).send(`course ${courseId} does not exist`);
-    });
-});
 app.listen(port, host, () => {
   console.log(`Listening at http://${host}:${port}`)
 });
